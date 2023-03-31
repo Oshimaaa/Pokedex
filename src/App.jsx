220dc038a60bcdf65d135945dc8561ea14d48053
Victor Fabier <[MY\_NAME@example.com](mailto:fabier.victor.1720@gmail.com)>
2023-03-31 10:59:58 +0200

    Création du props.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,25 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import MyTitle from "./components/MyTitle"
-import './App.css'
-import PokemonCard from "./components/PokemonCard"
-
+import "./App.css";
+import PokemonCard from "./components/PokemonCard"; 
 
 function App() {
   const pokemonList = [
     {
       name: "bulbasaur",
+
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
+
     {
       name: "mew",
     },
   ];
-
   const pokemon = pokemonList[0];
-
   return (
     <div className="App">
       <PokemonCard pokemon={pokemon} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
