7679add19a78473d73eaa50e93266e9987e60330
Victor Fabier <[MY\_NAME@example.com](mailto:fabier.victor.1720@gmail.com)>
2023-03-31 10:19:35 +0200

    Modification du components.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,29 @@
-const PokemonCard = () => {
-    const pokemonList = [
-      {
-        name: "bulbasaur",
-  
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      },
-  
-      {
-        name: "carapuce",
-      },
-    ];
-    const pokemon = pokemonList[0];
-    return (
-      <div>
-        <figure>
-          {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : "?"}
-          <figcaption>{pokemon.name}</figcaption>
-        </figure>
-      </div>
-    );
-  };
+function PokemonCard() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  const pokemon = pokemonList[1];
+
+  return (
+    <div>
+      <figure>
+        {pokemon.name === "bulbasaur" ? (
+          <img src={pokemon.imgSrc} />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    </div>
+  );
+}
 
 export default PokemonCard;
-
