a55afe4ff4091ad417b684ae4b92714c5b9fbf72
Victor Fabier <[MY\_NAME@example.com](mailto:fabier.victor.1720@gmail.com)>
2023-03-31 12:15:26 +0200

    Modification du props.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard"; 
+import React, { useState } from "react";
 
 function App() {
   const pokemonList = [
     {
-      name: "bulbasaur",
-
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-
-    {
-      name: "mew",
-    },
-  ];
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
   const pokemon = pokemonList[0];
   return (
-    <div className="App">
+    <div>
       <PokemonCard pokemon={pokemon} />
     </div>
   );
 }
 
+
+
 export default App;
+
